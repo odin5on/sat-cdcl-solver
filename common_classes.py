9f8246cdf618b2381f8e7943180ab555f4f7689a
mddutton <matthew-dutton@uiowa.edu>
2024-04-30 15:35:15 -0500

    sort things into a few classes

---
+++ common_classes.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+from typing import Iterator, List, Optional, Set
+
+# frozen to be hashable
+@dataclass(frozen=True)
+class Literal:
+    variable: int
+    negation: bool
+
+    def __repr__(self):
+        if self.negation:
+            return "¬" + str(self.variable)
+        else:
+            return str(self.variable)
+
+    def neg(self) -> "Literal":
+        """
+        Return the negation of this literal.
+        """
+        return Literal(self.variable, not self.negation)
+
+
+@dataclass
+class Clause:
+    literals: List[Literal]
+
+    def __repr__(self):
+        return '∨'.join(map(str, self.literals))
+
+    def __iter__(self) -> Iterator[Literal]:
+        return iter(self.literals)
+
+    def __len__(self):
+        return len(self.literals)
+
+    def __hash__(self):
+        x = 0 
+        for lit in self.literals:
+            x ^= hash(lit)
+        return x
+
+
+
+@dataclass
+class Formula:
+    clauses: List[Clause]
+    __variables: Set[int]
+
+    def __init__(self, clauses: List[Clause]):
+        """
+        Remove duplicate literals in clauses.
+        """
+        self.clauses = []
+        self.__variables = set()
+        for clause in clauses:
+            self.clauses.append(Clause(list(set(clause))))
+            for lit in clause:
+                var = lit.variable
+                self.__variables.add(var)
+
+    def variables(self) -> Set[int]:
+        """
+        Return the set of variables contained in this formula.
+        """
+        return self.__variables
+
+    def __repr__(self):
+        return " ∧ ".join(f"({clause})" for clause in self.clauses)
+
+    def __iter__(self) -> Iterator[Clause]:
+        return iter(self.clauses)
+
+    def __len__(self):
+        return len(self.clauses)
+
+
+@dataclass
+class Assignment:
+    value: bool
+    antecedent: Optional[Clause]
+    dl: int  # decision level
---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+import random
+import time
+from implementation import parse_dimacs_cnf, cdcl_solve
+
+if __name__ == "__main__":
+    # you might comment it to get inconsistent execution time
+    random.seed(5201314)
+
+    start_time = time.time()
+
+    if len(sys.argv) != 2:
+        print("Provide one DIMACS cnf filename as argument.")
+        sys.exit(1)
+
+    dimacs_cnf = open(sys.argv[1]).read()
+    formula = parse_dimacs_cnf(dimacs_cnf)
+    result = cdcl_solve(formula)
+
+    total_time = time.time() - start_time
+
+    if result:
+        assert result.satisfy(formula)
+        print("sat")
+        sorted_items = sorted(result.items(), key=lambda x: int(x[0]))
+        for var, assignment in sorted_items:
+            print(str(var) + " := " + str(assignment.value).lower())
+    else:
+        print("unsat")
+
+    print(f"Execution time: {total_time:.6f} seconds")  # Print the execution time
